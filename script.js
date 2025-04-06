1ecb5124fcd7aa5e51fa32b5fce20d647e348270
Abdoul03 <samabdoul03@gmail.com>
2025-04-06 15:53:50 +0000

    RightOk

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const couleurs = document.querySelector('.color')
+const telechargement = document.querySelector('.dow')
+//les deux triangles
+const triangle1 = document.querySelector('.triangle')
+const triangle2 = document.querySelector('.triangle2')
+//la partie gauche et droite
+const leftZone = document.querySelector('.left')
+const rightZone = document.querySelector('.right')
+
+let change = Math.floor(Math.random() * FFFFFF)
+
+couleurs.addEventListener('click', () => {
+
+})
