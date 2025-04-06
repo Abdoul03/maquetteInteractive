dcd42422c9ff33db64f6f0f7aca507308792dd42
Abdoul03 <samabdoul03@gmail.com>
2025-04-06 16:46:28 +0000

    Finished98%

--- script.js
+++ script.js
@@ -8,8 +8,18 @@ const triangle2 = document.querySelector('.triangle2')
 const leftZone = document.querySelector('.left')
 const rightZone = document.querySelector('.right')
 
-let change = Math.floor(Math.random() * FFFFFF)
+let change1 = Math.floor(Math.random() * 0xFFFFFF).toString(16)
+let change2 = Math.floor(Math.random() * 0xFFFFFF).toString(16)
+// console.log(change);
+
 
 couleurs.addEventListener('click', () => {
+    leftZone.style.backgroundColor = '#' + change1
+    rightZone.style.backgroundColor = '#' + change2
+})
 
+
+telechargement.addEventListener('click', () => {
+    const cv = document.querySelector('.contenerCV')
+    html2pdf().from(cv).save('cv.pdf')
 })
